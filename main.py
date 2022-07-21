0d0f38395bfba002ab04d1887165f50a36b92b62
philmcqueid <philmcqueid@gmail.com>
2022-07-21 20:43:04 -0300

    Firs Commit - Start coding

---
+++ main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+# Screen settings
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
+
+screen.exitonclick()
